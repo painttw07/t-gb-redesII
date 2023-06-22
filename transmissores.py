d36b951bb09c19d72cabe60651bcbdbc4e7b71df
Pamela <72814384+painttw07@users.noreply.github.com>
2023-06-22 18:54:42 -0300

    Update transmissores.py

--- transmissores.py
+++ transmissores.py
@@ -2,90 +2,56 @@ import time
 import random
 from threading import Thread
 
-class TransmitterData: #classe que representa o funcionamento de um transmissor
+class TransmitterData:
     def __init__(self, name, canal):
         self.name = name
         self.canal = canal
-        self.colidiu = False
+        self.retry_limit = 5
 
-    def transmite(self, data): #metodo tenta transmitir os dados como argumentos:
-        #verifica se tem comunicação livre (esta_livre).abs
-        #Se retornar true (está livre de fato) -> transmissão é feita atraves de transmite_dado
-        #transmite_dado chama o método receive em cada transmissor conectado
-        while True:
-            if self.canal.esta_livre():
-                print(f"{self.name} está transmitindo: {data}")
-                self.canal.transmite_dado(data, self)
-                break
+    def transmite(self, data):
+        retry_count = 0
+        while retry_count < self.retry_limit:
+            if self.canal.is_idle():
+                print(f"{self.name} está transmitindo:  {data}")
+                if self.canal.transmit_data(data, self):
+                    break
             else:
-                print(f"{self.name} Aguardando......")
-                time.sleep(1)
+                print(f"{self.name} Aguardando........")
+                self.canal.perform_backoff(retry_count)
+                retry_count += 1
 
     def receive(self, data):
-        if self.colidiu:
-            print(f"{self.name} está colidindo......")
-            self.colidiu = False
-        else:
-            print(f"{self.name} está recebendo {data}")
+        print(f"{self.name} received: {data}")
 
-    def detector_colisao(self):
-            time.sleep(0.5)  # Simulating detection delay
-            if self.canal.is_collision():
-                print(f"{self.name} colisão detectada >.< ")
-                self.colidiu = True
-
-class Canal: #classe que representa o canal de comunicação que é compartilhado entre os transmissores 1 e 2
+class Canal:
     def __init__(self):
-        self.transmitters = [] #array de transmissores que estão ligados a classe Canal na L51 e L52        
-        self.collision = False
+        self.transmitters = []
+        self.transmitting = None
 
-    def add_transmissor(self, transmissor):
-        self.transmitters.append(transmissor)
+    def add_transmissor(self, transmitter):
+        self.transmitters.append(transmitter)
 
-    def esta_livre(self):#verifica se algum dos transmissores está transmitindo no momento
-    #simulando um tempo de transmissão
-        for transmissor in self.transmitters:
-            if transmissor != self:
-                if self.transmitindo(transmissor):
-                    print(" OCUPADO ")
-                    return False 
-        print(" LIVRE ")
+    def is_idle(self):
+        return self.transmitting is None
+
+    def transmit_data(self, data, transmitter):
+        self.transmitting = transmitter
+        for receiver in self.transmitters:
+            if receiver != transmitter:
+                if self.detect_collision():
+                    print("Colisão detectada!")
+                    self.transmitting = None
+                    return False
+                receiver.receive(data)
+        self.transmitting = None
         return True
-        
 
-    def is_collision(self):
-        return self.collision
+    def detect_collision(self):
+        collision_prob = 0.2  # Probability of collision
+        return random.random() < collision_prob
 
-    def transmitindo(self, transmissor):
-        # Simulating transmission time
-        time.sleep(1)
-        return random.choice([True, False])
-
-    def transmite_dado(self, data, transmissor):
-    #Inicia-se a thread para cada receptor (cada transmissor conectado, exceto o transmissor que está transmitindo)
-    #e também uma thread separada para a detecção de colisões.
-    #Em seguida, aguarda-se a conclusão de todas as threads antes de definir o status de colisão no canal.
-        self.collision = False
-        threads = []
-        for recebedor in self.transmitters:
-            if recebedor != transmissor:
-                t = Thread(target=recebedor.receive, args=(data,))
-                threads.append(t)
-                t.start()
-
-        # Start collision detection thread
-        collision_thread = Thread(target=transmissor.detector_colisao)
-        collision_thread.start()
-        threads.append(collision_thread)
-
-        # Wait for all threads to complete
-        for t in threads:
-            t.join()
-
-        self.collision = any(transmissor.colidiu for transmissor in self.transmitters)
-
-    def perform_backoff(self, retry_count): #nao ta funcionando
+    def perform_backoff(self, retry_count):
         max_backoff = 2 ** retry_count
         backoff_time = random.uniform(0, max_backoff)
-        print(f"Aplicando backoff para {backoff_time}segundos")
+        print(f"Aplicando backoff para {backoff_time} s. ")
         time.sleep(backoff_time)
