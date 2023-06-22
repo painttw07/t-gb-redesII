ffd407b08910e69993d3ec913b9e5f283e1d9fc3
Pamela <72814384+painttw07@users.noreply.github.com>
2023-06-22 18:33:57 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from transmissores import TransmitterData, Canal
 
 # Criando o meio de comunicação
 canal = Canal()
+
 # Criando os transmissores
 t1 = TransmitterData("Transmissor 1", canal)
 t2 = TransmitterData("Transmissor 2", canal)
@@ -14,6 +15,7 @@ t3 = TransmitterData("Transmissor 3", canal)
 canal.add_transmissor(t1)
 canal.add_transmissor(t2)
 canal.add_transmissor(t3)
+
 # Testando a transmissão de dados
 t1.transmite("Bom dia do Transmissor 1!")
 t2.transmite("Boa noite do Transmissor 2!!!!!!")
